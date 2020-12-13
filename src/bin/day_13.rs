71819237ee38fa76364880c963e9701ef4108d3b
Giorgos Georgiou <giorgos.georgiou@outlook.com>
2020-12-13 13:26:08 +0200

    Abandoned day 13 part 2

--- src/bin/day_13.rs
+++ src/bin/day_13.rs
@@ -12,6 +12,8 @@ fn main() -> Result<()> {
     let bus_ids: Vec<_> = raw_ids.split(",").collect();
     let res = next_departure(timestamp, &bus_ids)?;
     println!("Result part 1 {}", res);
+    let res = golden_coin(&bus_ids);
+    println!("Result part 1 {}", res);
     Ok(())
 }
 
@@ -30,4 +32,44 @@ fn next_departure(timestamp: usize, bus_ids: &[&str]) -> Result<usize> {
 
 fn time_to_wait(timestamp: usize, bus_id: usize) -> usize {
     (timestamp as f32 / bus_id as f32).ceil() as usize * bus_id - timestamp
+}
+
+fn golden_coin(bus_ids: &[&str]) -> usize {
+    let (merged_offset, merged_id) = bus_ids
+        .iter()
+        .enumerate()
+        .filter(|(_, raw)| raw != &&"x")
+        .map(|(offset, id)| {
+            (offset, id.parse::<usize>().expect("Must be a number."))
+        })
+        .fold((0, 1), |(acc_offset, acc_id), (offset, id)| {
+            let lcm = num::integer::lcm(acc_id, id);
+            dbg!((merged_offset((acc_offset, acc_id), (offset, id)), lcm))
+        });
+    merged_id - merged_offset
+}
+
+fn merged_offset(
+    (offset_a, id_a): (usize, usize),
+    (offset_b, id_b): (usize, usize),
+) -> usize {
+    let mut candidate = 0;
+    loop {
+        candidate += id_a;
+        if candidate - offset_a % id_b == 0 {
+            return candidate + offset_b - offset_a;
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn examples() {
+        assert_eq!(golden_coin(&["x", "x", "3", "x", "5"]), 1);
+        assert_eq!(golden_coin(&["x", "x", "3", "7", "5"]), 46);
+        assert_eq!(golden_coin(&["17", "x", "13", "19"]), 3417);
+    }
 }
