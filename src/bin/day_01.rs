de998f77ac098f136a276b715eb312d4072cf10d
Giorgos Georgiou <giorgos.georgiou@outlook.com>
2020-12-18 21:19:05 +0200

    Merge day 1 for consistency

--- src/bin/day_01.rs
+++ src/bin/day_01.rs
@@ -1,8 +1,22 @@
 use aoc_2020::read_entries;
+use std::collections::HashSet;
 
 fn main() {
-    let entries: Vec<_> = read_entries::<usize>("./data/day-01.txt").collect();
+    let mut entry_set = HashSet::<u32>::new();
+    read_entries::<u32>("./data/day-01.txt").for_each(|entry| {
+        let complement = 2020 - entry;
+        if entry_set.contains(&complement) {
+            println!(
+                "Found {} and {}. Result is {}",
+                entry,
+                complement,
+                entry * complement
+            );
+        }
+        entry_set.insert(entry);
+    });
 
+    let entries = entry_set.iter().collect::<Vec<_>>();
     // Let's do a naive solution.
     for i in 0..entries.len() {
         let iv = entries[i];
