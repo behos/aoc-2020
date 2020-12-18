f52f6dda0af46248aa546eb6a710ee4766e46d72
Giorgos Georgiou <giorgos.georgiou@outlook.com>
2020-12-18 21:23:27 +0200

    Merge day 1 solutions

--- src/bin/day_01.rs
+++ src/bin/day_01.rs
@@ -1,32 +1,20 @@
 use aoc_2020::read_entries;
-use std::collections::HashSet;
 
 fn main() {
-    let mut entry_set = HashSet::<u32>::new();
-    read_entries::<u32>("./data/day-01.txt").for_each(|entry| {
-        let complement = 2020 - entry;
-        if entry_set.contains(&complement) {
-            println!(
-                "Found {} and {}. Result is {}",
-                entry,
-                complement,
-                entry * complement
-            );
-        }
-        entry_set.insert(entry);
-    });
-
-    let entries = entry_set.iter().collect::<Vec<_>>();
+    let entries = read_entries::<u32>("./data/day-01.txt").collect::<Vec<_>>();
     // Let's do a naive solution.
     for i in 0..entries.len() {
         let iv = entries[i];
         for j in i..entries.len() {
             let jv = entries[j];
+            if (iv + jv) == 2020 {
+                println!("Part 1: {} * {} = {}", iv, jv, iv * jv)
+            }
             for k in j..entries.len() {
                 let kv = entries[k];
                 if (iv + jv + kv) == 2020 {
                     println!(
-                        "Found result {} {} {} which has a product of {}",
+                        "Part 2: {} * {} * {} = {}",
                         iv,
                         jv,
                         kv,
